b543acf769c46be471d0bd76ca73fd5190adc205
kimja01 <kimja7045@naver.com>
2021-11-12 21:49:47 +0900

    Add loginForm

--- front/.next/static/chunks/webpack.js
+++ front/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b2ce19fb5b9248371820";
+/******/ 	var hotCurrentHash = "c63fe2e046ee2a075bd0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,26 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
 import { Menu, Input, Row, Col } from "antd";
+import styled from "styled-components";
+
+import UserProfile from '../components/UserProfile'
+import LoginForm from '../components/LoginForm'
+
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`
 
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
   return (
     <div>
       <Menu mode="horizontal">
-        <Menu.Item>
+        <Menu.Item key={0}>
           <Link href={"/"}>
             <a>노드버드</a>
           </Link>
         </Menu.Item>
-        <Menu.Item>
+        <Menu.Item key={1}>
           <Link href={"/profile"}>
             <a>프로필</a>
           </Link>
         </Menu.Item>
-        <Menu.Item>
-          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+        <Menu.Item key={2}>
+          <SearchInput enterButton />
         </Menu.Item>
-        <Menu.Item>
+        <Menu.Item key={3}>
           <Link href={"/signup"}>
             <a>회원가입</a>
           </Link>
@@ -29,7 +39,8 @@ const AppLayout = ({ children }) => {
 
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          왼쪽 메뉴
+          {/*왼쪽 메뉴*/}
+          {isLoggedIn ? <UserProfile/> : <LoginForm/>}
         </Col>
         <Col xs={24} md={12}>
           {children}
@@ -49,7 +60,7 @@ const AppLayout = ({ children }) => {
   );
 };
 
-AppLayout.PropTypes = {
+AppLayout.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
