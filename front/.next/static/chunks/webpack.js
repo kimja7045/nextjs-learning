7370ddb447d96ae568ebe762415b6f9f085ed905
kimja01 <kimja7045@naver.com>
2022-03-11 06:47:01 +0900

    Update reducer separation

--- front/.next/static/chunks/webpack.js
+++ front/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "02e24ad13afcbb6e0bf2";
+/******/ 	var hotCurrentHash = "9217c733389b4f949a0a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- front/reducers/index.js
+++ front/reducers/index.js
@@ -1,76 +1,32 @@
 import {HYDRATE} from 'next-redux-wrapper'
 
-const initialState = {
-    user: {
-        isLoggedIn: false,
-        user: null,
-        signUpData: {},
-        loginData: {},
-        name: 'zerocho',
-        age: 27,
-    },
-    post: {
-        mainPosts: [],
-    }
-}
-
-export const loginAction = (data) => {
-    return {
-        type: 'LOG_IN',
-        data
-    }
-}
-
-export const logoutAction = () => {
-    return {
-        type: 'LOG_OUT',
-    }
-}
-
-// async action creator
-
-// action creator
-export const changeNickname = (data) => {
-    return {
-        type: 'CHANGE_NICKNAME',
-        data
-    }
-}
+import user from './user' // userReducer
+import post from './post'
+import { combineReducers } from "redux";
+//
+// const initialState = {
+//     user: {
+//
+//     },
+//     post: {
+//     }
+// }
 
 // (이전상태, 액션) => 다음상태
-const rootReducer = (state = initialState, action) => {
-    switch (action.type){
-        // getInitialProps가 요즘에 거의 안쓰이고 getStaticProps, getServersideProps
-        // 이 2개를 쓰는걸로 ssr이 완전히 바뀌면서 새로 생김
-        case HYDRATE:
-            console.log('HYDRATE', HYDRATE)
-            return {...state, ...action.payload}
-        case 'CHANGE_NICKNAME':
-            return {
-                ...state,
-                name: action.data
-            }
-        case 'LOG_IN':
-            return {
-                ...state,
-                user: {
-                    ...state.user,
-                    isLoggedIn: true,
-                    user: action.data
-                }
-            }
-        case 'LOG_OUT':
-            return {
-                ...state,
-                user: {
-                    ...state.user,
-                    isLoggedIn: false,
-                    user: null
-                }
-            }
-        default:
-            return state;
-    }
-}
+const rootReducer = combineReducers({
+    index: (state = {}, action) => { // 원래 없어도되는데, 서버사이드렌더링을위해서 작성
+        switch (action.type){
+            // getInitialProps가 요즘에 거의 안쓰이고 getStaticProps, getServersideProps
+            // 이 2개를 쓰는걸로 ssr이 완전히 바뀌면서 새로 생김
+            case HYDRATE:
+                console.log('HYDRATE', HYDRATE)
+                return {...state, ...action.payload}
+            default:
+                return state;
+        }
+    },
+    user,
+    post,
+})
 
 export default rootReducer
